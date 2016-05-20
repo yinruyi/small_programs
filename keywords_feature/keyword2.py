3fed537bd1040436b599cea967917399ad3315c6
R. Yin <916280067@qq.com>
2016-05-20 21:46:24 +0800

    FIX：bug

--- keywords_feature/keyword2.py
+++ keywords_feature/keyword2.py
@@ -154,19 +154,8 @@ def textrank_feature():
         # temp.extend(words)
         result.append(temp)
         # break
-    preprocess().writeMatrix(result,'tfidf3.txt')    
+    preprocess().writeMatrix(result,'textrank.txt')    
 
 
 if __name__=='__main__':
     tfidf_feature()
-
-    # s = "此外，公司拟对全资子公司吉林欧亚置业有限公司增资4.3亿元，增资后，吉林欧亚置业注册资本由7000万元增加到5亿元。吉林欧亚置业主要经营范围为房地产开发及百货零售等业务。目前在建吉林欧亚城市商业综合体项目。2013年，实现营业收入0万元，实现净利润-139.13万元。"
-    # for x, w in jieba.analyse.extract_tags(s, withWeight=True):
-    #     print('%s %s' % (x, w))
-
-    # print('-'*40)
-    # print(' TextRank')
-    # print('-'*40)
-
-    # for x, w in jieba.analyse.textrank(s, withWeight=True):
-    #     print('%s %s' % (x, w))
